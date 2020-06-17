5cc5dada3c108f508791ee3fe4c0864572ba19a1
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2020-06-16 23:26:37 -0700

    upgrade on installs

--- inst/l4t-docker-context/Installers/pip-installs.bash
+++ inst/l4t-docker-context/Installers/pip-installs.bash
@@ -4,6 +4,6 @@ source $WORKON_HOME/r-reticulate/bin/activate
 alias make="make --jobs=`nproc`"
 
 echo "Installing 'numpy' and 'SymPy'"
-/usr/bin/time pip install numpy SymPy
+/usr/bin/time pip install --upgrade numpy SymPy
 
 pip list

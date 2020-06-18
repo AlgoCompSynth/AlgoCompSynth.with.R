2910c915782c779333523fbedf363c445897f48a
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2020-06-17 21:53:46 -0700

    tested and cleaned up

--- inst/l4t-docker-context/docker-pull.bash
+++ inst/l4t-docker-context/docker-pull.bash
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-docker pull nvcr.io/nvidia/l4t-base:r32.4.2
-docker pull znmeb/tarball:latest
-docker pull znmeb/l4t-r-audio:latest
+sudo docker pull znmeb/l4t-rstats:latest
+sudo docker pull znmeb/rserver-l4t-tgz-image:latest
+sudo docker pull nvcr.io/nvidia/l4t-base:r32.4.2

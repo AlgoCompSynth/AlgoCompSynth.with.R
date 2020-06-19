87a1db2469721736547be4187ce57e1bf69743f7
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2020-06-19 01:52:40 -0700

    just one image now!

--- inst/docker-contexts/docker-pull.bash
+++ inst/docker-contexts/docker-pull.bash
@@ -1,5 +1,4 @@
 #! /bin/bash
 
-sudo docker pull znmeb/l4t-rstats:latest
-sudo docker pull znmeb/rserver-l4t-tgz-image:latest
+sudo docker pull znmeb/l4t-algocompsynth:latest
 sudo docker pull nvcr.io/nvidia/l4t-base:r32.4.2

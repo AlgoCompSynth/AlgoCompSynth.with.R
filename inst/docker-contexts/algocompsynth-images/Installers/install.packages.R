3a5e7b48bb8529ebd0aaa4c446212ba995eef880
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2020-06-21 21:37:26 -0700

    get rid of tidyverse!

--- inst/docker-contexts/algocompsynth-images/Installers/install.packages.R
+++ inst/docker-contexts/algocompsynth-images/Installers/install.packages.R
@@ -1,17 +1,12 @@
 #! /usr/bin/env Rscript
 
 to_install <- c(
-  "data.table",
   "renv",
-  "reticulate",
-  "tidyverse"
+  "reticulate"
 )
 install.packages(to_install, quiet = TRUE)
 warnings()
 reticulate::py_discover_config()
 
-library(tidyverse)
-print("Installed")
-installed <- as_tibble(installed.packages()) %>% select(Package, Version); installed %>% print(n = Inf)
 print("Missing")
-print(setdiff(to_install, installed$Package))
+print(setdiff(to_install, as.data.frame(installed.packages())$Package))
